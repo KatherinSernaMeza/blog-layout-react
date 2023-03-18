406f7629fe75635ccf88d823745d3c3a3ed5b0d5
Katherin Serna <katherin.serna.m@gmail.com>
2023-03-17 19:28:07 -0700

    #Comment: file structuring.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Header from "./componentes/Header.jsx";
+import Banner from "./componentes/Banner.jsx";
+import Content from "./componentes/Content.jsx";
+import Footer from "./componentes/Footer.jsx";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Banner />
+      <Content />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/componentes/Banner.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div>This is the Banner!</div>;
+};
+
+export default Footer;
